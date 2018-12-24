855def3603943239f9fd67d1018861c0a656af6a
sahilkolwankar <sahilkolwankar@gmail.com>
2018-12-24 17:50:42 +0530

    Raw scraping code, initial commit

---
+++ parameters.py
@@ -0,0 +1,7 @@
+""" filename: paramaters.py """
+
+search_query = 'site:linkedin.com/in/ AND "python developer" AND "London"'
+file_name = 'results_file.csv'
+
+linkedin_username = 'sahilkolwankar@gmail.com'
+linkedin_password = 'napoleon1015800'
